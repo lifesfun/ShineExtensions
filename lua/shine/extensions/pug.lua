42bb73c90cd74594d0e37031936e140ce28bf2a3
lifesfun <dejong.nk@gmail.com>
2013-10-02 21:15:59 -0700

    updated skeleton code of pug.lua

--- lua/shine/extensions/pug.lua
+++ lua/shine/extensions/pug.lua
@@ -4,6 +4,8 @@ Shine Pug Plugin
 
 local Shine = Shine
 local Timer = Shine.Timer
+local FixArray = table.FixArray
+local Count = table.Count
 
 
 local Notify = Shared.Message
@@ -14,127 +16,107 @@ Plugin.Version = "0.1"
 Plugin.HasConfig = true
 Plugin.ConfigName = "Pug.json"
 Plugin.DefaultConfig = {
-    PugMode = true, -- Enabled Pug Mode
-    ForceTeams = false, --force teams to stay the same
-    MinPlayers = 8   
-    GameSize = 12
-    CaptainVoteLength = 30
+	PugMode = true, -- Enabled Pug Mode	
+	MinPlayers = 8   
+	GameSize = 12
+
+	VoteLength = 30
+	ChooseLength = 10	
 }
 
 Plugin.CheckConfig = true
 
---List of mods not compatible with tournament mode
-local BlacklistMods = {
-        [ "7e64c1a" ] = "Xenoswarm",
-        [ "6ed01f8" ] = "The Faded"
-}
-
-//saves Votes
-Plugin.Voted = {}
-Plugin.Votes = 0
-Plugin.Teams = {}
-Plugin.Captains = {}
-Plugin.Captain = 0
-Plugin.CurrentPick = nil
 Plugin.Readied = {}
-Plugin.Ready = 0
+Plugin.Voted = {}
+Plugin.Captains = {}
+Plugin.CurrentPick = nil
+Plugin.Teams = {}
  
 function Plugin:Initialise()
-    	local GetMod = Server.GetActiveModId
-
-	for i = 1, Server.GetNumActiveMods() do
-        	local Mod = GetMod( i ):lower()
-
-       		local OnBlacklist = BlacklistMods[ Mod ]
-	
-        	if OnBlacklist then
-        	    return false, StringFormat( "The Pug plugin does not work with %s.", OnBlacklist )
-       		end
-    	end
     
-        if self:CreateCommands() then Plugin:Nag() end
+        if self:CreateCommands() then Plugin:StartGame() end
 
         self.Enabled = true
 
         return true
 end
 
-local NextStartNag = 0
-
-function Plugin:CheckGameStart( Gamerules )
-
-	local State = Gamerules:GetGameState()
-
-	if self.Config.PugMode and Shared.GetEntitiesWithClassname("Player" ):GetSize() >= self.Config.MinPlayers then
-		 
-	end
-	if State == kGameState.PreGame or State == kGameState.NotStarted then
-	if NextStartNag < Time then
-		NextStartNag = Time + 30
-	
-		local Nag = self:GetStartNag()
-	
-		if not Nag then return false end
-		
-		self:SendNetworkMessage( nil, "StartNag", { Message = Nag }, true )
-	end
-	
-	return false
-	end
-end 
-
 function Plugin:Nag()
-  
+
+	
+		Plugin:Timer.Simple( 20 , function() 
+		
+			Plugin:NagMessage()
+			 		
+			local State = Gamerules:GetGameState()
+			
+			if not State ~= kGameState.Started then Plugin:NagMessage() end
+			
+		end )
+		
+		 
+function NagMessage( Client )
+
+	if Plugin.Captain[ Client ] then
+ 
+    		Shine:Notify( Client, "", "", "You have been chosen as a team captain. When it is your turn type !choose followed by their player name to pick your teammate.")
+		Shine:Notify( Client, "", "", "Your turn to choose...")
+		Shine:Notify( Client, "", "", "Waiting on more players to join.")
+		Shine:Notify( Client, "", "", "You have choosen, waiting on other captain.")
+
+	elseif Plugin.Voted[ Client ] then 
+		
+		
+			Shine:Notify( Client, "", "", "Teams are being chosen")
+			
+			Shine:Notify( Client, "", "", "You have voted. Waiting on other players to finish voting.")
+		
+	elseif  Plugin.Readied[ Client ] then 
+		 
+		Shine:Notify( Client, "", "", "Waiting on more players to join the pug.")
+
+		Shine:Notify( Client, "", "", "Time to choose your team captains. Type !vote followed by part of their player name to vote.")
+
+	else 
+	 
+		Shine:Notify( Client, "", "", "Pug Mode is enabled. Type !rdy to join the Pueggg!")
+
+	end
+
 end
 
-function Plugin:CheckVotes(Client, Player )
-    if self.Config.PugMode and Plugin.Voted >= MinPlayer then
-	if not Plugin.Ready <= self.Config.GameSize and Plugin.Readied[Client:GetUserId()] == false then
-		Shine:Notify( Client, "", "", "Pug Mode is enabled. Type !rdy to join the Pueggg!")
-        	if not Plugin.Readied[Client:GetUserId()] then Plugin.Readied[Client:GetUserId()]= true Plugin.Readied = Plugin.Readied + 1 end
-        elseif Plugin.Captains <= 2 and Plugin.Voted[Client:GetUserId()] == false then
-		Shine:Notify( Client, "", "", "Time to choose your team captains. Type !vote followed by part of their player name to vote.")
-        	if not Plugin.Voted[Client:GetUserId()] then Plugin.Voted[Client:GetUserId()]= true Votes = Votes + 1 end
-        if self.Config.PugMode and Client:GetUserId() == CurrentPick and playersOnTeams <= self.Config.GameSize then
+
+function Vote()
+
+	Plugin.Voted >= MinPlayer 
+	Plugin.Ready <= self.Config.GameSize and Plugin.Readied[Client:GetUserId()] == false 
+	Plugin.Readied[Client:GetUserId()] and Plugin.Readied[Client:GetUserId()]= true 
+	Plugin.Captains <= 2 and Plugin.Voted[Client:GetUserId()] == false 
+	Plugin.Voted[Client:GetUserId()] and Plugin.Voted[Client:GetUserId()]= true 
+	Client:GetUserId() == CurrentPick and playersOnTeams <= self.Config.GameSize 
+	Client:GetUserId() == Plugin.CurrentPick and playersOnTeams <= self.Config.GameSize 
+
+end
+
+
+function Plugin:ClientDisconnect(Client) 
+
+		Plugin.Readied[ Client:GetUserId() ]  
+		
+		Plugin.Voted[ Client:GetUserId() ] - 
+
+		Plugin.Captain[ Client:GetUserId() ]  
+	end
+end
+
+
+
+
+function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force, ShineForce )
             local Player = player:GetPlayer()
             local playerTeam = Client:GetPlayer():GetTeam():GetTeamNumber()
             if playerTeam ~= 0 then Shine:Notify( Client, "", "", "You can only choose players from the Ready Room") return end
             Gamerules:JoinTeam( Player, playerTeam, nil, true )
-        end
-	--check which captain has won the vote  at the end of the vote time if statment + for statment 2 condidtions
-	--add name to catpain list and captain online + if not greater 1
-	elseif self.Config.PugMode and Client:GetUserId() == Plugin.CurrentPick and playersOnTeams <= self.Config.GameSize then
-		Shine:Notify( Client, "", "", "Your turn...")
-	end
-
-    Shine:Notify( Client, "", "", "You have been chosen as a team captain. When it is your turn type !choose followed by their player name to pick your teammate.")
-   end
-return 0
-end
-
-function Plugin:StartGame( Gamerules )
-    Gamerules:ResetGame()
-    Gamerules:SetGameState( kGameState.Countdown )
-    Gamerules.countdownTime = kCountDownLength
-    Gamerules.lastCountdownPlayed = nil
-
-    for _, Player in ientitylist( Shared.GetEntitiesWithClassname( "Player" ) ) do
-        if Player.ResetScores then
-            Player:ResetScores()
-        end
-    end
-end
-    
-function Plugin:ClientDisconnect(Client)
-
-    if Plugin.Voted[Client:GetUserId()] then Plugin.Voted[Client:GetUserId()]= nil Plugin.Votes = Plugin.Votes - 1 end   
-
-end
-
---[[
-	Block players from joining teams in Pug mode
-]]
-function Plugin:JoinTeam( Gamerules, Player, NewTeam, Force, ShineForce )
     local id= Player:GetClient():GetUserId()
     
     --block f4 if forceteams is true
@@ -161,22 +143,22 @@ end
 
 function Plugin:CreateCommands()
 
-    local Ready = self:BindCommand( "sh_ready", { "rdy", "ready" }, CheckVotes(Client, Player ) )
-    	Ready:Help ("Join the Pug")
+    local Ready = self:BindCommand( "sh_ready" , { "rdy" , "ready" } , CheckVotes( Client ) )
+    	Ready:Help ( "Join the Pug" )
     
-    local Vote = self:BindCommand( "sh_vote", { "vote" }, CheckVotes(Client, Player  ) )
+    local Vote = self:BindCommand( "sh_vote", { "vote" }, CheckVotes( Client , Player  ) )
     	Choose:AddParam{ Type = "client"}    
-    	Choose:Help ("Type the name of the player to place him/her on your team.")
+    	Choose:Help ( "Type the name of the player to place him/her on your team." )
     
-    local Choose = self:BindCommand( "sh_choose", { "choose" }, CheckVotes(Client, Player ) )
+    local Choose = self:BindCommand( "sh_choose", { "choose" } , CheckVotes( Client , Player ) )
     	Choose:AddParam{ Type = "client"}    
-    	Choose:Help ("Type the name of the player to place him/her on your team.")
+    	Choose:Help ( "Type the name of the player to place him/her on your team." )
     
-    local Clearteams = self:BindCommand( "sh_clearteams", "clearteams", function()
+    local Clearteams = self:BindCommand( "sh_clearteams" , "clearteams" , function()
         self.Config.Teams = {}
         self:SaveConfig()         
-	end, true)
-   	Clearteams:Help("Removes all players from teams in config ")
+	end , true )
+   	Clearteams:Help( "Removes all players from teams in config" )
 end
 
 function Plugin:Cleanup()
