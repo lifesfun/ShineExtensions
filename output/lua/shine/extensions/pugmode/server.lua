9bae33289ed98f239f89cc216acac38547fb0fa9
lifesfun <dejong.nk@gmail.com>
2013-11-23 00:10:24 -0800

    Plugin:func

--- output/lua/shine/extensions/pugmode/server.lua
+++ output/lua/shine/extensions/pugmode/server.lua
@@ -1059,14 +1059,14 @@ function Plugin:CreateCommands()
 	SetTeamScoresCommand:AddParam{ Type = "number", Min = 0, Max = 255, Round = true, Optional = true, Default = 0 }
 	SetTeamScoresCommand:Help( "<Marine Score> <Alien Score> Sets the score for the marine and alien teams." )
 
-	local VoteOneCommand = self:BindCommand( "sh_vote1" , { "vote1" } , VoteOne , true )
+	local VoteOneCommand = self:BindCommand( "sh_vote1" , { "vote1" } , Plugin:VoteOne , true )
     	VoteOneCommand:AddParam{ Type = "string" ,  Optional = false , Default = "" }    
 
 
-	local VoteTwoCommand = self:BindCommand( "sh_vote2" , { "vote2" } , VoteTwo , true )
+	local VoteTwoCommand = self:BindCommand( "sh_vote2" , { "vote2" } , Plugin:VoteTwo , true )
     	VoteTwoCommand:AddParam{ Type = "string" ,  Optional = false , Default = "" }    
     
-	local ChooseCommand = self:BindCommand( "sh_choose" , { "choose" } , Choose  , true )
+	local ChooseCommand = self:BindCommand( "sh_choose" , { "choose" } , Plugin:Choose  , true )
     	ChooseCommand:AddParam{ Type = "string" , Optional = false ,  Default = "" }    
 
 	--reset pug Startpug
