b1fe8a4839516201f89c74d3e1c8cc831ec9e682
lifesfun <dejong.nk@gmail.com>
2013-11-23 16:36:48 -0800

    forgot to add change to VoteOne

--- output/lua/shine/extensions/pugmode/server.lua
+++ output/lua/shine/extensions/pugmode/server.lua
@@ -1059,7 +1059,7 @@ function Plugin:CreateCommands()
 	SetTeamScoresCommand:AddParam{ Type = "number", Min = 0, Max = 255, Round = true, Optional = true, Default = 0 }
 	SetTeamScoresCommand:Help( "<Marine Score> <Alien Score> Sets the score for the marine and alien teams." )
 
-	local VoteOneCommand = self:BindCommand( "sh_vote1" , { "vote1" } , pugmode:VoteOne , true )
+	local VoteOneCommand = self:BindCommand( "sh_vote1" , { "vote1" } , self.VoteOne , true )
     	VoteOneCommand:AddParam{ Type = "string" , Default = "" }    
 
 
