344e33eb6249ffeb7868eec8938ddd1509ad2ec2
lifesfun <dejong.nk@gmail.com>
2013-11-23 18:10:54 -0800

    id chagn

--- output/lua/shine/extensions/pugmode/server.lua
+++ output/lua/shine/extensions/pugmode/server.lua
@@ -126,7 +126,7 @@ function Plugin:GameStatus()
 		local TeamSize = self.Config.TeamSize 
 		local Waiting = StringFormat( "Waiting for the Pick up Game to begin for a %s V %s Pug" , TeamSize , TeamSize) 
 	
-		Shine:SendText( nil, Shine.BuildScreenMessage( 50, 0.5, 0.7, Waiting , 5, 255, 255, 255, 1, 3, 1 ) )
+		Shine:SendText( nil, Shine.BuildScreenMessage( 51, 0.5, 0.7, Waiting , 5, 255, 255, 255, 1, 3, 1 ) )
 
 	elseif self.GameStarted == true then
 
