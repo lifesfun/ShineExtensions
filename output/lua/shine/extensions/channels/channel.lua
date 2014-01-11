01ded4e2015118ba7c48f834706252aa91584cf0
lifesfun <dejong.nk@gmail.com>
2014-01-11 13:32:58 -0800

    moving channel obj to a file

---
+++ output/lua/shine/extensions/channels/channel.lua
@@ -0,0 +1,56 @@
+Channel = { 
+
+	self.Name = nil, 
+	self.Password = nil, 
+	self.Clients = {  
+
+		self.Name = nil,
+		self.Active = nil 
+	}
+}
+
+function Channel:new ( 0 )
+
+	o = o or {}
+	setmetable( o , self ) 
+	self._index = self
+	return 0
+end
+
+function Channel:GetName()
+
+	return self.Name
+end
+
+function Channel:Activate( Client , Active = false )
+
+	self.Clients[ Client ] = Active 
+end
+
+function Channel:CanAccess( Password )
+
+	if self.Password = Password then return true end
+end
+
+function Channel:AddToChannel( Client , ChannelClient )
+	
+	self.Clients[ Client ] = ChannelClient 	
+end
+
+function Channel:RemoveClient( Client )
+
+	local ChannelClient = self.Clients[ Client ]
+
+	if ChannelClient then 
+	
+		self.Clients[ Client ] = nil  
+		if not self.GetChannelClients() then self.Channel = nil end
+		return ChannelClient
+	end
+end
+
+function Channel:GetChannelClients() 
+	
+	return self.Clients
+end	
+
