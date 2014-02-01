4072a056e69a2b477b561d2d2185d3d7660af2f3
lifesfun <dejong.nk@gmail.com>
2014-01-31 17:24:07 -0800

    updates

--- output/lua/Player_Lift.lua
+++ output/lua/Player_Lift.lua
@@ -1,5 +1,6 @@
 Player.LiftOffset = Vector( 0 , 1 , 0 )
 Player.LiftTolerance = 0.28
+Player.LiftInterval = 0.5
 Player.LiftLastUse = nil
 Player.LiftID = nil
 
@@ -8,7 +9,7 @@ function Player:MinTime()
 	print( "hello" )
 	local time = Shared.GetTime()
 
-	if self.LiftLastUse and ( time < ( self.LiftLastUse + Lift.Interval ) ) then self.LiftLastUse = time 
+	if self.LiftLastUse and ( time < ( self.LiftLastUse + self.LiftInterval ) ) then self.LiftLastUse = time 
 
 	else self.LiftLastUse = time return true end
 end
@@ -18,15 +19,14 @@ function Player:LerkLift( target )
 print( "test" )
 	if kLiftDev then return true end
 	if target.LiftID then return end
-	if target:isa( "Gorge" ) then return true 
+	if target:isa( "Gorge" ) then print("gorgeis") return true 
 
-	elseif self:isa( "Gorge" ) and target:isa( "Lerk" ) then 
-	return true end
+	elseif self:isa( "Gorge" ) and target:isa( "Lerkis" ) then target:isa("Lerk" ) return true end
 end
 
 function Player:GetCanBeUsed( target , useSuccessTable )
 	
-	if kLiftEnabled then useSuccessTable.UseSuccess = true end
+	useSuccessTable.UseSuccess = true 
 end
 
 function Player:OnUse( target , elapsedTime , useSuccessTable )
@@ -34,12 +34,12 @@ function Player:OnUse( target , elapsedTime , useSuccessTable )
 print( "use" )
 	if not target then return end
 	if not kLiftEnabled then return end
-	if not self:LerkLift( target ) then return end
 
 	if not self:MinTime() then return end
-	
-	local selfID = self:GetId()
+	if not self:LerkLift( target ) then return end
+
 	local targetID = target:GetId()
+	local selfID = self:GetId()
 
 	--if I am used and I have my targets Id 
 	if self.LiftID and self.LiftID == targetID then 
@@ -51,53 +51,62 @@ print( "use" )
 
 		target:ResetLift() 
 	else
-		target:ResetLift() 
+
+print( target.LiftID )
 		target:SetLift( selfID ) 
 	end
 	useSuccessTable.UseSuccess = true
 end
 
+function Player:SetLift( id )
+
+print( "setlift" )
+	self:TriggerEffects( "alien_vision_On" )
+	if id then self.LiftID = id end
+print( self.LiftID )
+
+end
+
+function Player:ResetLift()
+
+print( "reset" )
+	self:TriggerEffects( "alien_vision_off" )
+	if self.LiftID then self.LiftID = nil end 
+print( self.LiftID )
+
+end
+
 function Player:UpdateMove( deltaTime )
 
+	if deltaTime <= 0 then return end
+
 	if not kLiftEnabled then return end
-print( "update" )
 	if not self.LiftID then return end
 
 	local target = Shared.GetEntity( self.LiftID ) 
 	if target then self:Lift( target )  
 
 print( "lift" )
+print( self.LiftID )
+
 	else self:ResetLift() end
 end
 
----use functions 
-function Player:SetLift( id )
-
-print( "set" )
-	self:TriggerEffects( Lift.OnSound )
-	if id then self.LiftID = id end
-end
-
-function Player:ResetLift()
-
-print( "reset" )
-	self:TriggerEffects( Lift.OffSound )
-	if self.LiftID then self.LiftID = nil end 
-end
-
----Lift function 
 function Player:Lift( target )
 	
 	local attachPoint = self:GetOrigin() + self.LiftOffset	
 	local targetOrigin = target:GetOrigin() 
 
-	local distance = ( attachPoint - targetOrigin ):GetLength()
+	local distance = attachPoint - targetOrigin 
+	local distanceXY = distance:GetLength()
+
 	local min = attachPoint - self.LiftTolerance
 	local max = attachPoint + self.LiftTolerance
 
-	if distance > min and distance < max then return end
+print( targetOrigin )
+	if distanceXY > min and distanceXY < max then return end
+print( attachPoint )
 	
-	local moveDir = GetNormalizedVector( attachPoint - targetOrigin )
-	target:SetOrigin( targetOrigin + moveDir * distance )
+	target:SetOrigin( attachPoint )
 end
 
