5d61618219c39feeb0648be21947e8f0be269a72
lifesfun <dejong.nk@gmail.com>
2014-02-11 17:04:25 -0800

    new settings

--- output/lua/Lift.lua
+++ output/lua/Lift.lua
@@ -1,127 +1,90 @@
-Lift = { 
+Lift = {
 	Enabled = true,
 	Dev = false,
-
+	Interval = 0.5,
 	OnSound = "alien_vision_on",
-	OffSound = "alien_vision_off"
-
-	DefaultOffset = Vector( 0 , 1 , 1 ),
-	DefaultTolerance = 0.28 
+	OffSound = "alien_vision_off",
+	Offset = Vector( 0 , 2.8 , 0 ),
+	Tolerance = 0.28 
 }
 
---first array is lifter, second is lifted
-Lift.Entity = { 
-	[ "Skulk" ]  = Vector( 0 , 1 , 0 ), 
-	[ "Lerk" ] = Vector( 0 , 1 , 0 ),
-	[ "Gorge" ] = Vector( 0 , 1 , 0 ),	
-	[ "Fade" ] = Vector( 0 , 2 , 0 ),
-	[ "Onos" ] = Vector( 0 , 3 , 0 ),
-	[ "Marine" ] = Vector( 0 , 2 , 0 ),
-	[ "Exo" ] = Vector( 0 , 3 , 0 )
-}
+function Lift:All()
 
-function Lift:UseLift( lifter , lifted ) 
+	local gamerules = GetGamerules()
+	if Shared.GetCheatsEnabled() or self.Dev or not gamerules:GetGameStarted() then return true end
+end
+
+function Lift:Liftable( lifter , lifted )
 
 	if not self.Enabled then return end
+	if self:All() then return true end
 
-	local lifterID = lifter:GetID()
-	local liftedID = lifted:GetID()
-
-	if lifter.LiftedID and lifter.LiftedID == liftedID  then 
-
-		self:Detach( lifter , lifted )
-
-	elseif lifted.LiftedID and lifted.LiftedID == lifterID then  
-
-		self:Detach( lifter , lifted )
-	else 
-		self:Attach( lifter , lifted )
-	end
+	if lifted.LiftedID or lifted.LifterID then return end 
+	if not lifter:GetIsAlive() or not lifted:GetIsAlive() then return end
+	
+	if lifted:isa( "Gorge" ) and lifter:isa( "Lerk" ) then return true end
 end
 
-function Lift:IsLiftable( lifter , lifted  )
+function Lift:UseLift( user , used )
 
-	if not self.Enabled then return end
-	if self.kDev then return true end	
+	local userID = user:GetId()
+	local usedID = used:GetId()
+	--if I am used and I have my targets Id 
+	if used.LiftID and used.LiftID == userID then self:Detach( used ) return true
 
-	if not target:GetIsAlive() then return end 
-	if lifter:LiftedID then return end 
-
-	if lifter:isa( "Gorge" ) then return true 
-
-	elseif lifted:isa( "Gorge" ) and lifter:isa( "Lerk" ) then return true end
+	--if I am used and target has my id then reset 
+	elseif user.LiftID and user.LiftID == usedID then self:Detach( user) return true
+	
+	elseif self:Liftable( user , used ) then self:Attach( used , userID ) return true end
 end
 
-function Lift:Attach( lifter ,  lifted )
+function Lift:Attach( lifted , lifterID)
 
-	if not self:IsLiftable( lifter , lifted ) then return false end
-
-	lifter.TriggerEffects( Lift.OnSound )
-	lifted.physicsBody:SetGravityEnabled( false ) 	
-
-	lifter.LiftedLastVelocity = Vector( 0 , 0 , 0 )
-	lifter.LiftedID = lifted:GetId() 
-
-	lifter.Offset = self:GetOffset( lifter )
-	lifted.Offset = self:GeOffset( lifted )
+	lifted:TriggerEffects( self.OnSound )
+	if lifterID then lifted.LiftID = lifterID end
 end
 
-function Lift:Detach( lifter , lifted )
+function Lift:Detach( lifted )
 
-	lifter.TriggerEffects( Lift.OffSound )
-	lifted.physicsBody:SetGravityEnabled( true ) 	
-	lifter.LiftedID = nil 
-	lifter.Offset = nil
-	lifted.Offset = nil
+	lifted:TriggerEffects( self.OffSound )
+	if lifted.LiftID then lifted.LiftID = nil end
 end
 
-function Lift:GetOffset( entity )
+function Lift:KeepLifting( lifted , lifterID )
 
-	local name = self:GetType( entity ) 
-	if not name then return self.DefaultOffset end
+	if not self.Enabled then self:Detach( lifted) return end
+	
+	local lifter = Shared.GetEntity( lifterID ) 
+	if not lifter then self:Detach( lifted ) return end
 
-	local offset = self.Entity[ name ] 
-	if not offset then return self.DefaultOffset end
-
-	return offset
+	if self:All() then self:Lift( lifter , lifted ) 
+	
+	elseif lifter:GetIsAlive() and lifted:GetIsAlive() then self:Lift( lifter, lifted ) 
+	
+	else self:Detach( lifter ) end
 end
 
-function Lift:GetType( entity )
-	return entity.kmap
+function Lift:Lift( lifter , lifted )
+
+	local tolerance = lifter.LiftTolerance 
+	if not tolerance then tolerance = self.Tolerance end
+	
+	local offset = lifter.LiftOffset
+	if not offset then offset = self.Offset end
+	
+	local offsetXY = offset:GetLength() 
+	local minTolerance = offsetXY - tolerance
+	local maxTolerance = offsetXY + tolerance
+	
+	local attachPoint = lifter:GetOrigin()  + offset
+	local liftedOrigin = lifted:GetOrigin()
+	
+	local distanceXY = ( attachPoint - liftedOrigin ):GetLength()
+	if minTolerance < distanceXY and distanceXY < maxTolerance then return end
+	
+   -- local moveDir = GetNormalizedVector( attachPoint - liftedOrigin)
+
+	lifted:SetOrigin( attachPoint )
+--	lifted:SetOrigin( liftedOrigin + moveDir * distanceXY )
 end
-
-function Lift:GetTolerance( entity )
-	return entity.kMinimumPlayerVelocity
-end
-
-function Lift:Process( lifter , lifted , deltaTime )
-
-	if not self.Enabled then return end
-	if deltaTime <= 0 then return true end 
-
-	local liftedOrigin = lifted:GetOrigin() 
-	local lifterOrigin = lifter:GetOrigin() 
-	local destination = lifted.Offset + lifter.Offset + lifterOrigin 
-
-	local distance = destination - liftedOrigin
-	local distanceXY = distance:GetLength()
-
-	local tolerance = self:GetTolerance( lifted )  
-	if not tolerance then tolerance = self.DefaultTolerance end
-
-	local min = distanceXY - tolerance 
-	local max = distanceXY + tolerance
-
-	if distanceXY > min and distanceXY < max then return end 
-
-	local velocity = distance / deltaTime 
-	self:Lift( lifted , destination , velocity )
-end
-
-function Lift:Lift( lifted , destination , velocity )
-
-	lifted:SetOrigin( destination ) 
-	lifted:SetVelocity( velocity ) 
-end
-
 
--- output/lua/shine/extensions/lift/server.lua
+++ output/lua/shine/extensions/lift/server.lua
@@ -1,5 +1,8 @@
 --A mod that provides Lift functions and more!
+Script.Load( "lua/Lift.lua" )
+
 local Shine = Shine
+local GetOwner = Server.GetOwner
 
 local Plugin = {} 
 Plugin.Version = "1.0"
@@ -10,13 +13,7 @@ Plugin.ConfigName = "Lift.json"
 Plugin.DefaultConfig = {
 
 	Default = true,
-<<<<<<< HEAD
-	DevMode = false,
-	Tolerance = 0.53,
-	Offset = Vector( 0 , 0 , 0 )
-=======
 	Dev = false
->>>>>>> lerklift
 }
 
 Plugin.CheckConfig = true
@@ -24,16 +21,10 @@ Plugin.DefaultState = true
 
 function Plugin:Initialise()
 
-<<<<<<< HEAD
-	kEnabled = self.Config.Default 
-	kDev = self.Config.kDev
-	kOffset = self.Config.kOffset
-	kTolerance = self.Config.kTolerance
-=======
-	kLiftEnabled = self.Config.Default 
-	kLiftDev = self.Config.Dev
->>>>>>> lerklift
-
+	Lift.Enabled = self.Config.Default 
+	Lift.Dev = self.Config.Dev
+    self.Started = false
+	
 	self:CreateCommands()
 	self.Enabled = true
 	return true
@@ -41,98 +32,59 @@ end
 
 function Plugin:Notify( Player , String , Format , ... ) 
 
-	Shine:NotifyDualColour( Client , 0 , 100 , 255 , "LiftBot" , 255 ,  255 , 255 , String , Format , ... ) 
+	Shine:NotifyDualColour( Player , 0 , 100 , 255 , "LiftBot" , 255 ,  255 , 255 , String , Format , ... ) 
 end
 
-function Plugin:ClientConfirmConnect( client )
+function Plugin:PostJoinTeam( Gamerules, Player, OldTeam, NewTeam, Force, ShineForce ) 
+	if OldTeam == 0 then 
+		local client = GetOwner( Player )
+		self:TellPlayers(  client , Gamerules ) 
+	end
+end
+function Plugin:CheckGameStart( gamerules )
 
-	self:TellPlayers( client ) 
+	if gamerules:GetGameStarted() and self.Started == false then 
+	
+		self:TellPlayers(  nil , gamerules ) 
+		self.Started = true
+	end
 end
 
-function Plugin:TellPlayers( client )
+function Plugin:TellPlayers( client , gamerules )
 
-<<<<<<< HEAD
-		self:Notify( Client , "The Lift mod is enabled!" )
-		self:Notify( Client , "Use e to lift up a gorge as a lerk." )
-		self:Notify( Client , "Use e to lift up any living player as a gorge" )
-=======
-	if not kLiftEnabled then return end
-	self:Notify( client , "The Lift mod is Enabled!" )
-	self:Notify( client , "Use e to lift up a gorge as a lerk." )
-	self:Notify( client , "Use e to lift up any living player as a gorge." )
->>>>>>> lerklift
+	if not Lift or not Lift.Enabled then return end
+	if not gamerules:GetGameStarted()  then return end
+	self:Notify( client , "I am enabled :D" )
+	self:Notify( client , "Press E to use Lift" )
+	self:Notify( client , "Gorges can pick up any class, Lerks can pick up Gorges." )
 end
 
 function Plugin:CreateCommands()
 
 	local function SetLiftEnabled( client , enable )
 
-<<<<<<< HEAD
-		kEnabled = Enable 
-	 	self.Config.Default = Enabled 
-		self:SaveConfig()
-		self:Notify( nil , "Lift set to %s" , true , Enabled  )	
-	end
-	local LiftCommand = self:BindCommand( "lft" , "lft" , Lift )
-	LiftCommand:AddParam{ Type = "boolean" , Optional = true , Default = true }
-	LiftCommand:Help( "Sets if should be enabled" )
-=======
-		kLiftEnabled = enable 
+		Lift.Enabled = enable 
 		self:TellPlayers( nil ) 
 	end
 	local LiftEnabledCommand = self:BindCommand( "lft" , "lft" , SetLiftEnabled )
 	LiftEnabledCommand:AddParam{ Type = "boolean" , Optional = true , Default = true }
 	LiftEnabledCommand:Help( "Sets if lift is enabled." )
->>>>>>> lerklift
 
 	local function SetLiftDev( client, enable )
 
-<<<<<<< HEAD
-		kLift = Enable 
-		self.Config.kDev = Enabled
-		self:SaveConfig()
-		self:Notify( nil , "LiftDev is set to %s" , true , Enabled )	
-	end
-	local LiftDevCommand = self:BindCommand( "lftdev" , "lftdev" , SetLiftDev )
-	LiftDevCommand:AddParam{ Type = "boolean" , Optional = true , Default = true }
-=======
-		kLiftDev = enable 
-		self:Notify( nil , "LiftDev is set to %s" , true , kLiftDev )	
+		Lift.Dev = enable 
+		self:Notify( nil , "LiftDev is set to %s" , true , enable )	
 	end
 	local LiftDevCommand = self:BindCommand( "lftdev" , "lftdev" , SetLiftDev )
 	LiftDevCommand:AddParam{ Type = "boolean" , Optional = true , Default = false }
->>>>>>> lerklift
 	LiftDevCommand:Help( "Sets to  dev mode" )
-
-	local function SetOffset( Client , y , x , z  )
-
-		kOffset = Vector(  x  , y  , z )
-		self.Config.kOffset = KOffset
-		self:SaveConfig()
-		self:Notify( Client , "Lift Offset: Y %s  X %s  Z %s" , true , y , x , z  )
-	end
-	local LiftOffsetCommand = self:BindCommand( "lftos" , "lftos" , SetOffset )
-	LiftOffsetCommand:AddParam{ Type = "number" , Optional = true Default = 0 }
-	LiftOffsetCommand:AddParam{ Type = "number" , Optional = true , Default = 0 }
-	LiftOffsetCommand:AddParam{ Type = "number" , Optional = true , Default = 0 }
-	LiftOffsetCommand:Help( "Sets the offset y x z" )
-
-	local function SetTolerance( Client, tolerance )
-
-		kTolerance = tolerance 
-		self.Config.kTolerance = tolerance
-		self:SaveConfig()
-		self:Notify( Client  , "Lift Tolerance is set to %s " , true , tolerance )
-	end
-	local LiftToleranceCommand = self:BindCommand( "lftol" , "lftol" , SetTolerance )
-	LiftToleranceCommand:AddParam{ Type = "number" , Optional = true , Default = 0.28 }
-	LiftToleranceCommand:Help( "Sets the tolerance" )
-
 end
 
 function Plugin:Cleanup()
 
-	kLiftEnabled = false 
+	Lift.Enabled = nil 
+	Lift.Dev = nil
+	self.Started = nil
 	self.Enabled = false
 	self.BaseClass.Cleanup( self )
 end
